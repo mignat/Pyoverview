af0a82955a43f32f9acc3a46eb78660eaaa99162
Marius Ignat <marius.ignat@tradeshift.com>
2019-07-25 17:55:54 +0300

    Added things

--- containers/web/www/templates/settings/settings-ui-connections.php
+++ containers/web/www/templates/settings/settings-ui-connections.php
@@ -6,8 +6,7 @@
  * Time: 15:30
  */
 if ($_SESSION['permissions'] != 1) {
-    include("../error.php");
-    #header("Location: http://$_SERVER[HTTP_HOST]/templates/error.php?errortype=Access_denied"); /* Redirect browser */
+    header("Location: http://$_SERVER[HTTP_HOST]/templates/error.php?errortype=Access_denied"); /* Redirect browser */
     exit();
 }
 ?>
