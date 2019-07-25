f960b014a239455cd7bc4080f1aa4a84a835d616
Marius Ignat <marius.ignat@tradeshift.com>
2019-07-25 18:46:45 +0300

    Added things

--- containers/web/www/templates/Dashboard/dashboard-ui-reports.php
+++ containers/web/www/templates/Dashboard/dashboard-ui-reports.php
@@ -6,10 +6,52 @@
  * Time: 17:37
  */
 
-require "../../lib/table_creator.php";
 
-
-$test = new table_creator("SELECT * FROM `pyover_users`");
-$test->run_query();
 ?>
-<h1>TEST </h1>
+
+
+<script>
+
+    var Chart = require('../../scripts/Chart.bundle');
+    var ctx = $('#myChart');
+    var myChart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+            datasets: [{
+                label: '# of Votes',
+                data: [12, 19, 3, 5, 2, 3],
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)'
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true
+                    }
+                }]
+            }
+        }
+    });
+
+
+
+</script>
+<canvas id="myChart" width="400" height="400"></canvas>
