23bd6d6d6e0737a53b11edd567e66e76c092fe37
Marius Ignat <marius.ignat@tradeshift.com>
2019-07-26 18:31:07 +0300

    Added things

--- containers/web/www/templates/settings/settings-ui-database.php
+++ containers/web/www/templates/settings/settings-ui-database.php
@@ -10,3 +10,8 @@ if ($_SESSION['permissions'] != 1) {
     header("Location: http://$_SERVER[HTTP_HOST]/templates/error.php?errortype=Access_denied"); /* Redirect browser */
     exit();
 }
+
+?>
+
+<iframe frameborder="0" id="monitoring_frame"
+        src="../../phpmyadmin/index.php"></iframe>
