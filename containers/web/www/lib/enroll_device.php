04d7d80e26fa338de2428272cadde46cc19538da
Marius Ignat <marius.ignat@tradeshift.com>
2019-07-27 01:07:28 +0300

    Added things

--- containers/web/www/lib/enroll_device.php
+++ containers/web/www/lib/enroll_device.php
@@ -6,11 +6,11 @@ $data = json_decode($json,true);
 
 //  Start enroll process
 
-//$stationName = $data['STATION']['name'];
-//$stationLocation = $data['STATION']['location'];
-//$stationDescription = $data['STATION']['description'];
+$stationName = $data['STATION']['name'];
+$stationLocation = $data['STATION']['location'];
+$stationDescription = $data['STATION']['description'];
 
-sqlexec("INSERT INTO `pyover_devices` (`UID`, `name`, `location`, `Description`, `status`, `last_contact`) VALUES (uuid(), 'Pytest', 'Sala 1', 'CEVA', 'N/A', NULL)", null,true);
+sqlexec("INSERT INTO `pyover_devices` (`UID`, `name`, `location`, `Description`, `status`, `last_contact`) VALUES (uuid(), '$stationName', '$stationLocation', '$stationDescription', 'N/A', NULL)", null,true);
 
 echo "Done";
 
