a13a6e17633cdda6f5570893df21f7e31de66361
Marius Ignat <marius.ignat@tradeshift.com>
2019-07-27 03:14:15 +0300

    Added things

--- containers/web/www/lib/enroll_device.php
+++ containers/web/www/lib/enroll_device.php
@@ -4,26 +4,26 @@ require("sqlQuery.php");
 
 if (isset($_GET['verify_uuid']){
     $uuid = $_GET['verify_uuid'];
-    $query = sqlexec("SELECT * FROM `pyover_devices` WHERE `UID`=\'408fc14d-afd6-11e9-b06a-b827eb5d7d84\'");
-    echo $query[0]["UID"];
+    $query = sqlexec("SELECT * FROM `pyover_devices` WHERE `UID`=\'$uuid\'");
+    $query[0]["name"];
 
-}
+}else {
 
-$json = file_get_contents('php://input');
-$data = json_decode($json,true);
+    $json = file_get_contents('php://input');
+    $data = json_decode($json, true);
 
 //  Start enroll process
 
-$stationName = $data['STATION']['name'];
-$stationLocation = $data['STATION']['location'];
-$stationDescription = $data['STATION']['description'];
+    $stationName = $data['STATION']['name'];
+    $stationLocation = $data['STATION']['location'];
+    $stationDescription = $data['STATION']['description'];
 
-$uuid = sqlexec("select uuid()")[0]['uuid()'];
+    $uuid = sqlexec("select uuid()")[0]['uuid()'];
 
 
-$enroll_query = sqlexec("INSERT INTO `pyover_devices` (`UID`, `name`, `location`, `Description`, `status`, `last_contact`) VALUES ('$uuid', '$stationName', '$stationLocation', '$stationDescription', 'N/A', NULL)", null,true);
-echo "$uuid\n";
-
+    $enroll_query = sqlexec("INSERT INTO `pyover_devices` (`UID`, `name`, `location`, `Description`, `status`, `last_contact`) VALUES ('$uuid', '$stationName', '$stationLocation', '$stationDescription', 'N/A', NULL)", null, true);
+    echo "$uuid\n";
+}
 
 
 
