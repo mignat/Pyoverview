29d32cd70133b7c975600deb57c848510dcc7833
Marius Ignat <marius.ignat13@gmail.com>
2019-07-28 14:47:23 +0300

    added things

--- containers/web/www/lib/data_acquisition.php
+++ containers/web/www/lib/data_acquisition.php
@@ -1,5 +1,9 @@
 <?php
 require("sqlQuery.php");
 
-$sql = "INSERT INTO `pyover_usetime` (`device_uid`, `start_time`, `end_time`) VALUES (\'af3e2304-b11c-11e9-af1b-b827eb5d7d84\', \'123131\', \'12312312\')";
-sqlexec($sql,null,true);
+
+try {
+    sqlexec("INSERT INTO `pyover_usetime` (`device_uid`, `start_time`, `end_time`) VALUES (\'af3e2304-b11c-11e9-af1b-b827eb5d7d84\', \'123131\', \'12312312\')", null, true);
+} catch (Exception $e) {
+    echo 'Caught exception: ', $e->getMessage(), "\n";
+}
