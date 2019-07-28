2d48673e77b2afa3c92f3d250c65e9da2e5def75
Marius Ignat <marius.ignat13@gmail.com>
2019-07-28 14:39:03 +0300

    added things

--- containers/web/www/lib/data_acquisition.php
+++ containers/web/www/lib/data_acquisition.php
@@ -1,15 +1,5 @@
 <?php
 require("sqlQuery.php");
 
-
-$json = file_get_contents('php://input');
-$data = json_decode($json, true);
-echo "Test";
-
-$uuid = $data['uuid'];
-$start = $data['start_time'];
-$stop  = $data['stop_time'];
-
-echo "Test2";
 $sql = "INSERT INTO `pyover_usetime` (`device_uid`, `start_time`, `end_time`) VALUES (\'af3e2304-b11c-11e9-af1b-b827eb5d7d84\', \'123131\', \'12312312\')";
 sqlexec($sql,null,true);
