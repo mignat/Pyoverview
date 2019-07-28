2e5fe06c0816f6492e6334d9ab369394445d84dc
Marius Ignat <marius.ignat13@gmail.com>
2019-07-28 14:58:49 +0300

    added things

--- containers/web/www/lib/data_acquisition.php
+++ containers/web/www/lib/data_acquisition.php
@@ -4,4 +4,4 @@ require("sqlQuery.php");
 
 
 $sql = "INSERT INTO `pyover_usetime` (`device_uid`, `start_time`, `end_time`) VALUES (\'a7ca7fa9-b12b-11e9-abd4-b827eb5d7d84\', \'3123\', \'123123\')";
-$x = sqlexec($sql);
+$x = sqlexec($sql,null,true);
