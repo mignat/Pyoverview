d61f77eaa644f8e510e45c423b8e411a827b7161
Marius Ignat <marius.ignat13@gmail.com>
2019-07-28 14:29:38 +0300

    Update data_acquisition.php

--- containers/web/www/lib/data_acquisition.php
+++ containers/web/www/lib/data_acquisition.php
@@ -9,5 +9,7 @@ echo "Test";
 $uuid = $data['uuid'];
 $start = $data['start_time'];
 $stop  = $data['stop_time'];
+
+echo "Test2";
 $sql = "INSERT INTO `pyover_usetime` (`device_uid`, `start_time`, `end_time`) VALUES (\'{$uuid}\', \'{$start}\', \'{$stop}\')";
 sqlexec($sql);
