3f4b8fcec7e7530c7c7777a2c824e2fe5e4b887b
Marius Ignat <marius.ignat13@gmail.com>
2019-07-28 14:11:48 +0300

    Update data_acquisition.php

--- containers/web/www/lib/data_acquisition.php
+++ containers/web/www/lib/data_acquisition.php
@@ -6,7 +6,7 @@ $json = file_get_contents('php://input');
 $data = json_decode($json, true);
 
 
-$statonUUID = $data['uuid'];
+$uuid = $data['uuid'];
 $start = $data['start_time'];
 $stop  = $data['stop_time'];
-sqlexec("INSERT INTO `pyover_usetime` (`device_uid`, `start_time`, `end_time`) VALUES ('$statonUUID', '$start', '$stop')",null,true);
+sqlexec("INSERT INTO `pyover_usetime` (`device_uid`, `start_time`, `end_time`) VALUES ('$uuid', '$start', '$stop')");
