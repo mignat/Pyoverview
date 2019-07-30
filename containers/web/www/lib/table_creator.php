c5a7e0b29b49dd3bf34faeb0993a693f4cf74b2e
Marius Ignat <marius.ignat13@gmail.com>
2019-07-30 20:33:43 +0300

    Update table_creator.php

--- containers/web/www/lib/table_creator.php
+++ containers/web/www/lib/table_creator.php
@@ -16,34 +16,37 @@ class table_creator {
         $this->data = sqlexec($this->query);
         return ($this->data);
     }
-    function gen_table()
-    {
+
+    function genTable(){
+
+        if ($this->data != "") {
+            $collums = array_keys($this->data[0]);
 
 
-    }
 
-    function genTable($query){
+            echo "<table class=\"table table-hover\">";
+            echo "<thead>";
+            echo "<tr>";
+            foreach ($collums as $col){
+                echo "<th scope=\"col\">$col</th>";
+            }
+            echo "</tr>";
+            echo "</thead>";
+            echo "<tbody>";
+            echo "ROW HERE";
+            echo "<tr>";
+            echo "<th scope=\"row\">1</th>";
+            echo "<td>Mark</td>";
+            echo "</tr>";
+            echo "</tbody>";
+            echo "</table>";
+        }else{
 
-        echo "<table class=\"table table-hover\">";
-        echo "<thead>";
-        echo "<tr>";
-        echo "<th scope=\"col\">#</th>" ;
-        echo "</tr>";
-        echo "</thead>";
-        echo "<tbody>";
-        echo "ROW HERE";
-        echo "<tr>";
-        echo "<th scope=\"row\">1</th>";
-        echo "<td>Mark</td>" ;
-        echo "</tr>";
-        echo "</tbody>";
-        echo "</table>";
-
+            echo "Missing Query !";
+        }
 
     }
 
 
 }   $x = new table_creator("SELECT * from pyover_users");
-    $collums_names = array_keys($x->run_query()[1]);
-
-    echo $collums_names[1];
+    $x->genTable();
