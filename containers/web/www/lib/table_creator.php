a71a5b0136de9443a53e3878ecfab889724720b5
Marius Ignat <marius.ignat13@gmail.com>
2019-07-30 19:57:14 +0300

    Update table_creator.php

--- containers/web/www/lib/table_creator.php
+++ containers/web/www/lib/table_creator.php
@@ -16,8 +16,27 @@ class table_creator {
         $this->data = sqlexec($this->query);
         echo $this->data;
 
+    function genTable($query){
+
+        echo "<table class=\"table table-hover\">";
+        echo "<thead>";
+        echo "<tr>";
+        echo "<th scope=\"col\">#</th>" ;
+        echo "</tr>";
+        echo "</thead>";
+        echo "<tbody>";
+        echo "ROW HERE";
+        echo "<tr>";
+        echo "<th scope=\"row\">1</th>";
+        echo "<td>Mark</td>" ;
+        echo "</tr>";
+        echo "</tbody>";
+        echo "</table>"
 
 
     }
 
-}
+    }
+
+}   $x = new table_creator("SELECT * from pyover_users");
+    echo $x->run_query();
