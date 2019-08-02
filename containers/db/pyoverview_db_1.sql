212260ad19fde3fadf5b8117ab35b75dc3a4b235
Marius Ignat <marius.ignat13@gmail.com>
2019-08-02 09:24:39 +0300

    Added table-creator class

--- containers/db/pyoverview_db_1.sql
+++ containers/db/pyoverview_db_1.sql
@@ -1,12 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.0.1
+-- version 4.8.2
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost
--- Generation Time: Jul 30, 2019 at 06:59 PM
--- Server version: 10.3.15-MariaDB-1
--- PHP Version: 7.3.4-2
-
+-- Host: pyoverview_db_1
+-- Generation Time: Nov 20, 2018 at 11:00 AM
+-- Server version: 8.0.11
+-- PHP Version: 7.2.6
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
 START TRANSACTION;
@@ -23,38 +22,26 @@ SET time_zone = "+00:00";
 --
 
 -- --------------------------------------------------------
-
---
--- Table structure for table `pyover_audit`
---
-
-CREATE TABLE `pyover_audit` (
-  `user_uid` int(11) NOT NULL,
-  `severity` int(11) NOT NULL,
-  `Message` varchar(50) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- --------------------------------------------------------
-
+CREATE DATABASE IF NOT EXISTS `pycom_dashboard` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci;
+USE `pycom_dashboard`;
 --
 -- Table structure for table `pyover_devices`
 --
 
 CREATE TABLE `pyover_devices` (
-  `UID` varchar(36) NOT NULL,
   `name` varchar(30) NOT NULL,
-  `location` varchar(20) NOT NULL,
   `Description` varchar(150) DEFAULT NULL,
   `status` varchar(30) DEFAULT 'N/A',
   `last_contact` timestamp NULL DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Dumping data for table `pyover_devices`
 --
 
-INSERT INTO `pyover_devices` (`UID`, `name`, `location`, `Description`, `status`, `last_contact`) VALUES
-('8f5e4757-b132-11e9-abd4-b827eb5d7d84', 'Pycom_43', 'Sala1', 'test device', 'N/A', NULL);
+INSERT INTO `pyover_devices` (`name`, `Description`, `status`, `last_contact`) VALUES
+('Pycom_1', 'Testing this shit', 'OK', '2018-08-22 00:00:00'),
+('Pycom_2', 'This is for testing', 'N/A', '2018-08-22 00:00:00');
 
 -- --------------------------------------------------------
 
@@ -68,77 +55,26 @@ CREATE TABLE `pyover_users` (
   `password_hash` varchar(150) NOT NULL,
   `privileges` varchar(1) NOT NULL,
   `api_token` varchar(150) NOT NULL,
-  `timestamp` timestamp NOT NULL DEFAULT current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Dumping data for table `pyover_users`
 --
 
-INSERT INTO `pyover_users` (`username`, `full_name`, `password_hash`, `privileges`, `api_token`, `timestamp`) VALUES
-('mignat', 'Marius Ignat', '$2y$10$h7pp1RyYfcT0LiWnx4rKTOA2GkdikJZfNmgRELvU081oPB2YYRJxG', '1', '$2y$10$wVeODq0YVAhcREuPZnC6h.OEVswsocv04QlhcXYFgUYnxNEKz09se', '2019-07-24 13:46:00'),
-('tnanu', 'Tiberiu Nanu', '$2y$10$VkUe.fUb3MlOkCWQ5XxGseCRtUlztO0IWQKyDDhrdq3sDPZkLINLC', '0', '$2y$10$VkUe.fUb3MlOkCWQ5XxGseCRtUlztO0IWQKyDDhrdq3sDPZkLINLC', '2019-07-25 14:42:07');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `pyover_usetime`
---
-
-CREATE TABLE `pyover_usetime` (
-  `device_uid` varchar(36) NOT NULL,
-  `start_time` varchar(10) NOT NULL,
-  `end_time` varchar(10) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `pyover_usetime`
---
-
-INSERT INTO `pyover_usetime` (`device_uid`, `start_time`, `end_time`) VALUES
-('8f5e4757-b132-11e9-abd4-b827eb5d7d84', '1564327451', '1564327455'),
-('8f5e4757-b132-11e9-abd4-b827eb5d7d84', '1564327476', '1564327479'),
-('8f5e4757-b132-11e9-abd4-b827eb5d7d84', '1564336487', '1564336489'),
-('8f5e4757-b132-11e9-abd4-b827eb5d7d84', '1564336513', '1564336515');
+INSERT INTO `pyover_users` (`username`, `full_name`, `password_hash`, `privileges`, `api_token`) VALUES
+('mignat', 'Marius Ignat', '$2y$10$h7pp1RyYfcT0LiWnx4rKTOA2GkdikJZfNmgRELvU081oPB2YYRJxG', '1', '$2y$10$wVeODq0YVAhcREuPZnC6h.OEVswsocv04QlhcXYFgUYnxNEKz09se'),
+('tnanu', 'Nanu Tiberiu', '$2y$10$EfTdSfxSbrdzAej9phGAsego/Sb1EhwknBFouT/A9Zwq3sh1Rcrjy', '0', '$2y$10$EfTdSfxSbrdzAej9phGAsego/Sb1EhwknBFouT/A9Zwq3sh1Rcrjy');
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `pyover_audit`
---
-ALTER TABLE `pyover_audit`
-  ADD PRIMARY KEY (`user_uid`);
-
---
--- Indexes for table `pyover_devices`
---
-ALTER TABLE `pyover_devices`
-  ADD PRIMARY KEY (`UID`),
-  ADD UNIQUE KEY `name` (`name`);
-
---
 -- Indexes for table `pyover_users`
 --
 ALTER TABLE `pyover_users`
   ADD PRIMARY KEY (`username`);
-
---
--- Indexes for table `pyover_usetime`
---
-ALTER TABLE `pyover_usetime`
-  ADD KEY `device_uid` (`device_uid`);
-
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `pyover_usetime`
---
-ALTER TABLE `pyover_usetime`
-  ADD CONSTRAINT `pyover_usetime_ibfk_1` FOREIGN KEY (`device_uid`) REFERENCES `pyover_devices` (`UID`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
--- containers/web/www/lib/table_creator.php
+++ containers/web/www/lib/table_creator.php
@@ -1,48 +1,74 @@
 <?php
 
 
-class table_creator {
+class table_creator
+{
     private $query;
     private $data;
+    private $option_menu;
+    private $option_entries;
+    private $line_numbers;
 
-    function __construct($query)
+    function __construct($query,$option_menu=false,$line_numbers=false)
     {
         $this->query = $query;
+        $this->option_menu = $option_menu;
+        $this->line_numbers = $line_numbers;
     }
 
-    function run_query ()
+    function run_query()
     {
         require_once "sqlQuery.php";
         $this->data = sqlexec($this->query);
         return ($this->data);
     }
 
-    function genTable(){
+    function addOption($option){
+        $this->option_entries[] = $option;
+    }
+
+    function genTable()
+    {
+        $this->run_query();
 
         if ($this->data != "") {
-            $collums = $this->data[0];
-            foreach (array_keys($collums) as $keys ){
-                echo $keys;
-            }
+            $data = $this->data;
+            $raw_collums = array_keys($data[0]);
+            $collumns = array();
 
-            echo "";
-            echo "<table class=\"table table-hover\">";
-            echo "<thead>";
+            echo "<link rel=\"stylesheet\" href=\"https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css\" integrity=\"sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO\" crossorigin=\"anonymous\">";
+            echo "<table class=\"table table-hover align-content-center\" align=\"center\">";
+            echo "<thead class=\"thead-dark\">";
             echo "<tr>";
-            foreach ($keys as $col){
-                echo "<th scope=\"col\">$col</th>";
+            foreach ($raw_collums as $col) {
+                if (!is_numeric($col)) {
+                    echo "<th scope=\"col\">$col</th>";
+                    $collumns[] = $col;
+                }
+                if ($this->option_menu){
+                    echo "<td>";
+                    echo "<div class='btn-group'>";
+
+                    echo "</div>";
+                    echo "</td>";
+                }
             }
             echo "</tr>";
             echo "</thead>";
             echo "<tbody>";
-            echo "ROW HERE";
-            echo "<tr>";
-            echo "<th scope=\"row\">1</th>";
-            echo "<td>Mark</td>";
-            echo "</tr>";
+            foreach ($data as $row) {
+                echo "<tr scope=\"row\">";
+                foreach ($collumns as $tdcol){
+                echo "<td>$row[$tdcol]</td>";
+                }
+
+
+                echo "</tr>";
+            }
+
             echo "</tbody>";
             echo "</table>";
-        }else{
+        } else {
 
             echo "Missing Query !";
         }
@@ -50,6 +76,8 @@ class table_creator {
     }
 
 
-}   $x = new table_creator("SELECT * from pyover_users");
-    $x->run_query();
-    $x->genTable();
+}
+
+$x = new table_creator("SELECT `username` AS `USERNAME`,`full_name` AS `NAME`,`privileges` AS `PRIVILEGES` FROM `pyover_users` WHERE 1");
+$x->run_query();
+$x->genTable();
