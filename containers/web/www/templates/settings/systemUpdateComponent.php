28fc2eb82d85c77666c5a722d0966168b99d5e02
Marius Ignat <marius.ignat13@gmail.com>
2019-12-02 02:12:08 +0200

    Moved systemUpdate compontent to settings template folder

--- containers/web/www/templates/settings/systemUpdateComponent.php
+++ containers/web/www/templates/settings/systemUpdateComponent.php
@@ -1,6 +1,6 @@
 <?php
 ?>
-<link rel="stylesheet" href="style.css">
+<link rel="stylesheet" href="../../components/systemUpdate/style.css">
 <script src="scripts/systemupdate.js"></script>
 <button type="button" class="btn btn-primary btn-dark" data-toggle="modal" data-target="#systemUpdateModal" >System Update</button>
 
