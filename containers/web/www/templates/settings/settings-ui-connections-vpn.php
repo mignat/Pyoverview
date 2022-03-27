7f01e4139603a524c9f31dcf2ba748a812a9282c
Marius Ignat <marius.ignat@tradeshift.com>
2022-03-27 10:29:41 +0300

    Update VPN section to Tunnel

--- containers/web/www/templates/settings/settings-ui-connections-vpn.php
+++ containers/web/www/templates/settings/settings-ui-connections-vpn.php
@@ -5,8 +5,37 @@ if ($_SESSION['permissions'] != 1) {
     exit();
 }
 ?>
-<button type="button" class="btn btn-primary btn-dark" data-toggle="modal" data-target="#ModalVpn" >Settings</button>
-<div class="modal fade" id="ModalVpn" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+
+<script>
+    function restartAjax() {
+        $.ajax({
+            type: "GET",
+            url: "../../lib/be_connections.php",
+            data: "operation=restart&service=ssh_tunnel",
+        });
+    }
+    function startAjax() {
+        $.ajax({
+            type: "GET",
+            url: "../../lib/be_connections.php",
+            data: "operation=start&service=ssh_tunnel",
+        });
+    }
+    function stopAjax() {
+        $.ajax({
+            type: "GET",
+            url: "../../lib/be_connections.php",
+            data: "operation=stop&service=ssh_tunnel",
+        });
+    }
+</script>
+
+
+<button type="button" onclick="startAjax()" class="btn btn-primary btn-success">Start</button>
+<button type="button" onclick="stopAjax()" class="btn btn-primary btn-danger">Stop</button>
+<button type="button" onclick="restartAjax()" class="btn btn-primary btn-warning">Restart</button>
+<button type="button" class="btn btn-primary btn-dark" data-toggle="modal" data-target="#ModalTunnel" >Settings</button>
+<div class="modal fade" id="ModalTunnel" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
     <div class="modal-dialog" role="document">
         <div class="modal-content">
             <div class="modal-header">
@@ -16,18 +45,18 @@ if ($_SESSION['permissions'] != 1) {
                 </button>
             </div>
             <div class="modal-body">
-                <form id="Form" action="../../lib/be_connections.php?operation=vpn_update" method="post">
+                <form id="Form" action="../../lib/be_connections.php?operation=wifi_update" method="post">
                     <div class="form-group">
-                        <label for="ap_name">VPN Server</label>
-                        <input type="text" class="form-control" id="vpn_server"  placeholder="VPN Server">
-                        <label for="exampleInputFile">Certificate</label>
-                        <input type="file" class="form-control-file" id="exampleInputFile" aria-describedby="fileHelp">
+                        <label for="ap_name">SSID</label>
+                        <input type="text" class="form-control" id="ap_name"  placeholder="AP Name">
+                        <label for="ap_pass">Password</label>
+                        <input type="password" class="form-control" id="ap_pass" placeholder="Password">
                     </div>
                 </form>
             </div>
             <div class="modal-footer">
                 <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                <button type="submit" form="Form" id="vpn-submit" class="btn btn-primary">Save & Apply</button>
+                <button type="submit" form="Form" id="wifi-submit" class="btn btn-primary">Save & Apply</button>
             </div>
         </div>
     </div>
